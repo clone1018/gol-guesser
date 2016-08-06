7294c9137d438b39cb54258ec151136998f94cc6
clone1018 <luke@axxim.net>
2016-08-05 23:03:17 -0500

    apparently I can super simplify this

--- main.go
+++ main.go
@@ -2,48 +2,23 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"path"
-	"runtime"
-
 	"log"
 	"os/exec"
+	"path"
+	"runtime"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	cipher := "rc2-cfb"
-	key := "tracer"
-
-	file, err := saveGuess(key)
-	if err != nil {
-		panic(err)
-	}
+	key := "&& ls"
 
 	hashPath := path.Join("hashes", "test.b64")
-	fmt.Println(hashPath)
 
-	out, err := exec.Command("openssl", cipher, "-d", "-a", "-in", hashPath, "-kfile", file).Output()
+	out, err := exec.Command("openssl", cipher, "-d", "-a", "-in", hashPath, "-pass", "pass:"+key).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s\n", out)
-}
-
-func saveGuess(guess string) (string, error) {
-	content := []byte(guess)
-	file, err := ioutil.TempFile("/tmp", "guess")
-	if err != nil {
-		log.Fatal(err)
-		return "", err
-	}
-
-	_, err = file.Write(content)
-	if err != nil {
-		log.Fatal(err)
-		return "", err
-	}
-
-	return file.Name(), nil
 }
